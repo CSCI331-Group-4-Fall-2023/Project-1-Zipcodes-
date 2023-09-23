30cfa5c02311b952adc4dc5b0cc3950aa90b6b33
Tristan Adams <98128375+TristanA1016@users.noreply.github.com>
2023-09-22 21:20:17 -0500

    Update ZipApp.cpp
    finished version, update tags and comments later

--- ZipApp.cpp
+++ ZipApp.cpp
@@ -4,7 +4,7 @@
 #include <sstream> // For stringstream
 #include <string> // For getline
 #include <fstream> // For file reading
-//authors Tristan Adams and Preston Betz
+//authors Tristan Adams, Preston Betz, and achary Sunder
 using namespace std;
 
 /**
@@ -40,13 +40,71 @@ int main() {
         // cout << "State " << z << " was set to " << zipTable[z].getID() << endl;
         z++;
     }
-
+    zipTable[0].setID("AL");
     // Call buff class and waterfall comparisons
+
+    ZipcodeBuffer zipHolder;
+    ifstream inFile2("test.csv");
+     if (!inFile2) {
+        cout << "Error opening file" << endl;
+        return -1;
+    }
+
+    //loop that will feed the array piece by piece
+    while (getline(inFile2, line)) { 
+        zipHolder.setFromFile(line);
+
+        //itterate through the array and match the state ID
+        for (int i = 0; i < 50; i++) {
+
+            //populate stateRow if its unused
+            if(zipHolder.getState() == zipTable[i].getID()){
+                if(zipTable[i].getFresh() == true){
+                    zipTable[i].setEast(zipHolder.getLongitude());
+                    zipTable[i].setEastZ(zipHolder.getZipcode());
+                    zipTable[i].setWest(zipHolder.getLongitude());
+                    zipTable[i].setWestZ(zipHolder.getZipcode());
+                    zipTable[i].setNorth(zipHolder.getLatitude());
+                    zipTable[i].setNorthZ(zipHolder.getZipcode());
+                    zipTable[i].setSouth(zipHolder.getLatitude());
+                    zipTable[i].setSouthZ(zipHolder.getZipcode());
+                    zipTable[i].setFresh(false);
+                     break;
+                // comparisons for if the row has been touched
+                }else{
+                    if(zipTable[i].getEast() > zipHolder.getLongitude()){
+                        zipTable[i].setEast(zipHolder.getLongitude());
+                        zipTable[i].setEastZ(zipHolder.getZipcode());
+                    }
+                    if(zipTable[i].getWest() < zipHolder.getLongitude()){
+                        zipTable[i].setWest(zipHolder.getLongitude());
+                        zipTable[i].setWestZ(zipHolder.getZipcode());
+                    }
+                    if(zipTable[i].getNorth() < zipHolder.getLatitude()){
+                        zipTable[i].setNorth(zipHolder.getLatitude());
+                        zipTable[i].setNorthZ(zipHolder.getZipcode());
+                    }
+                    if(zipTable[i].getSouth() > zipHolder.getLatitude()){
+                        zipTable[i].setSouth(zipHolder.getLatitude());
+                        zipTable[i].setSouthZ(zipHolder.getZipcode());
+                    }
+                     break;
+                }
+                
+               
+            }
+            
+        }
+    }
+
+
 
     // Print the final table
     cout << "-------Table of States Final-------" << endl;
     for (int i = 0; i < 50; i++) {
-        cout << zipTable[i] << endl;
+        if(zipTable[i].getFresh() == 0){
+            cout << zipTable[i] << endl;
+        }
     }
 
     return 0;
